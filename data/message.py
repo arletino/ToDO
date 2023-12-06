1efe479310d15dbfa066e94685b52d5948dff9dc
arletino <titkov@nxt.ru>
2023-12-07 00:45:53 +0300

    Class message first step

---
+++ data/message.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+class Message:
+    '''Класс для создания сообщения'''
+    def __init__(self, message: str, id = 0) -> None:
+        self.id = id
+        self.date_create = datetime.now().strftime("%d/%m/%y %H:%M:%S")
+        self.date_change = datetime.now().strftime("%d/%m/%y %H:%M:%S")
+        self.message = message
+    
+    def get_id(self):
+        return self.id
+    
+    def set_id(self, id):
+        self.id = id
+    
+    def set_message(self, message):
+        self.message = message
+        self.date_change = datetime.now().strftime("%d/%m/%y %H:%M:%S")
+
+    def __str__(self):
+        return f'{self.id} {self.date_create} {self.date_change} \n {self.message}'
